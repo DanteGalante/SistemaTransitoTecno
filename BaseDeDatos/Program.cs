5dcbd68bac02afe35d602e79b2ae89fecb5c13ed
Dan <olveradan@hotmail.com>
2021-05-26 22:18:21 -0500

    Actualización de la base de datos

---
+++ BaseDeDatos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD.ConexionBD;
+
+namespace BaseDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection conn = null;
+                conn = ConexionBD.GetConnection();
+                if (conn != null)
+                {
+                    Console.WriteLine("Se pudo conectar asies");
+                    Console.WriteLine(conn.ConnectionString);
+                    Console.ReadLine();
+                }
+
+            }
+            catch (SqlException exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
