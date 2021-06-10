e90b47e090e73b653ddc4a31a0112198ae5a41a2
Dan <olveradan@hotmail.com>
2021-06-10 10:32:22 -0500

    Funcionalidad de la ventana ConsutlarInfoVehiculo
    Navegabilidad de los botones y llenado del datagrid

--- SistemaDelegacionesMunicipales/Vista/ConsultarInfoVehiculo.xaml.cs
+++ SistemaDelegacionesMunicipales/Vista/ConsultarInfoVehiculo.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BaseDeDatos;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,55 @@ namespace SistemaDelegacionesMunicipales.Vista
     /// </summary>
     public partial class ConsultarInfoVehiculo : Window
     {
+        List<Vehiculo> vehiculos = new List<Vehiculo>();
+
         public ConsultarInfoVehiculo()
         {
             InitializeComponent();
+            LlenarTabla();
+        }
+
+        private void LlenarTabla()
+        {
+            using (BDTransitoEntities bdTransito = new BDTransitoEntities())
+            {
+                try
+                {
+                    foreach (Vehiculo vehiculoBD in bdTransito.Vehiculos)
+                    {
+                        vehiculos.Add(vehiculoBD);
+                    }
+
+                    dg_vehiculos.ItemsSource = vehiculos;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error en la conexion con la base de datos","Error",MessageBoxButton.OK);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void btn_regresar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_modificar_Click(object sender, RoutedEventArgs e)
+        {
+            ModificarRegistroVehiculo nuevaVentana = new ModificarRegistroVehiculo();
+            nuevaVentana.ShowDialog();
+        }
+
+        private void btn_registrar_Click(object sender, RoutedEventArgs e)
+        {
+            RegistroVehiculo nuevaVentana = new RegistroVehiculo();
+            nuevaVentana.ShowDialog();
+        }
+
+        private void btn_eliminar_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
