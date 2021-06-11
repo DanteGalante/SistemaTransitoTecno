c2257f6df4fd76338504ac501b8947f4fd5f143f
Dan <olveradan@hotmail.com>
2021-06-11 03:04:43 -0500

    Funcionalidad de la ventana ConsultarInfoConductorWindow
    Se llena el datagrid y se corrige un problema con el binding de la fecha de nacimiento

--- SistemaDelegacionesMunicipales/Vista/ConsultarInfoConductorWindow.xaml.cs
+++ SistemaDelegacionesMunicipales/Vista/ConsultarInfoConductorWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BaseDeDatos;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,33 @@ namespace SistemaDelegacionesMunicipales.Vista
     /// </summary>
     public partial class ConsultarInfoConductorWindow : Window
     {
+        BDTransitoEntities bdTransito = new BDTransitoEntities();
+        List<Conductor> conductores = new List<Conductor>();
         public ConsultarInfoConductorWindow()
         {
             InitializeComponent();
+            LlenarTabla();
+        }
+        
+        /**
+         * Este metodo se encarga de llenar el datagrid dg_conductor con informacion de los conductores sacada de la base de datos
+         */
+        private void LlenarTabla()
+        {
+            try
+            {
+                foreach (Conductor conductorBD in bdTransito.Conductores)
+                {
+                    conductores.Add(conductorBD);
+                }
+
+                dg_conductor.ItemsSource = conductores;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error en la conexion con la base de datos", "Error", MessageBoxButton.OK);
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
